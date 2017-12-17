99e06419721dd6b91042c7c9675c13609060e7d8
Scott Hummel <scotthummel@icloud.com>
2017-12-17 12:20:32 -0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Green Valley Sale</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        .tab-pane {
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <h1 class="text-center">Green Valley Sale</h1>
+
+    <p class="text-center">Going fast! Quality home furnishings, clothes, knickknacks, kitchen ware, lots more! Call Julie at 703-981-4235 for more information. All items in excellent condition. Barely used! <strong style="color: red;">Check back regularly. New items being added all the time!</strong></p>
+
+    <br />
+
+    <div>
+
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#furniture" aria-controls="furniture" role="tab" data-toggle="tab">Furniture</a></li>
+            <li role="presentation"><a href="#knickknacks" aria-controls="knickknacks" role="tab" data-toggle="tab">Knickknacks</a></li>
+            <li role="presentation"><a href="#clothes" aria-controls="clothes" role="tab" data-toggle="tab">Clothes</a></li>
+            <li role="presentation"><a href="#electronics" aria-controls="electronics" role="tab" data-toggle="tab">Electronics</a></li>
+            <li role="presentation"><a href="#misc" aria-controls="misc" role="tab" data-toggle="tab">Miscellaneous</a></li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="furniture">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/6.JPG" class="img-responsive"/>
+                        <p>High-end wall unit/entertainment center</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/19z.JPG" class="img-responsive"/>
+                        <p>Rust chair (dog not included).</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/24.JPG" class="img-responsive"/>
+                        <p>Small end table.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/28.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/33.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/34.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/36.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/37.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/38.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="knickknacks">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/1.JPG" class="img-responsive"/>
+                        <p>Mata Ortiz bowl in stand</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/2.JPG" class="img-responsive"/>
+                        <p>Mata Ortiz bowls</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/3.JPG" class="img-responsive"/>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/5.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/11.JPG" class="img-responsive"/>
+                        <p>Ceramic vase</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/18.JPG" class="img-responsive"/>
+                        <p>Ceramic urn</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/25.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/26.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/27.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/25.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/26.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/27.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/29.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/31.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/32.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/35.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="clothes"></div>
+            <div role="tabpanel" class="tab-pane" id="electronics">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/7.JPG" class="img-responsive"/>
+                        <p>Samsung flat screen TV</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="/images/4.JPG" class="img-responsive"/>
+                        <p>Bose high-end receiver and 3-disc changer.</p>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="misc">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/39.JPG" class="img-responsive"/>
+                        <p>Urn</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script><script src="js/bootstrap.min.js"></script>
+</body>
+</html>
