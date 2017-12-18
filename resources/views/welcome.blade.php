2e5a068fb0bd2f0ab35dcda2f31b6178cc9bca7c
Scott Hummel <scotthummel@icloud.com>
2017-12-18 16:19:26 -0700

    Final changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
 <div class="container">
     <h1 class="text-center">Green Valley Sale</h1>
 
-    <p class="text-center">Going fast! Quality home furnishings, clothes, knickknacks, kitchen ware, lots more! Call Julie at 703-981-4235 for more information. All items in excellent condition. Barely used! <strong style="color: red;">Check back regularly. New items being added all the time!</strong></p>
+    <p class="text-center">Going fast! Quality home furnishings, clothes, knickknacks, kitchen ware, lots more! Call Julie at 703-981-4235 for more information. All items in excellent condition. Barely used! Non-priced items -- best offer. All prices negotiable. Other items available that are not pictured. <strong style="color: red;">New items being added all the time! Check back regularly.</strong></p>
 
     <br />
 
@@ -52,7 +52,7 @@
                     </div>
                     <div class="col-md-4">
                         <img src="/images/19z.JPG" class="img-responsive"/>
-                        <p>Rust chair (dog not included).</p>
+                        <p>Rust chair (dog not included). $200.</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/24.JPG" class="img-responsive"/>
@@ -62,11 +62,11 @@
                 <div class="row">
                     <div class="col-md-4">
                         <img src="/images/45.JPG" class="img-responsive"/>
-                        <p>Hand-caned antique chair.</p>
+                        <p>Hand-caned antique chairs from late 1800s/early 1900s. One of two. $75.</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/47.JPG" class="img-responsive"/>
-                        <p>Antique, dovetailed dresser.</p>
+                        <p>Antique, walnut dovetailed ches of drawers. Late 1800s/early 1900s era. $400</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/49.JPG" class="img-responsive"/>
@@ -76,21 +76,21 @@
                 <div class="row">
                     <div class="col-md-4">
                         <img src="/images/28.JPG" class="img-responsive"/>
-                        <p>Antique dresser with dovetailed drawers.</p>
+                        <p>Antique walnut wash stand with dovetailed drawers. $250.</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/33.JPG" class="img-responsive"/>
-                        <p>Comfortable chair.</p>
+                        <p>Comfortable chair. $200.</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/34.JPG" class="img-responsive"/>
-                        <p>Jennifer Sofas hide-a-bed.</p>
+                        <p>Jennifer Sofas hide-a-bed. $200.</p>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4">
                         <img src="/images/36.JPG" class="img-responsive"/>
-                        <p>Contemporary kitchen table and chairs.</p>
+                        <p>Contemporary kitchen table and chairs. $125.</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/38.JPG" class="img-responsive"/>
@@ -98,18 +98,28 @@
                     </div>
                     <div class="col-md-4">
                         <img src="/images/48.JPG" class="img-responsive"/>
-                        <p>Antique hutch with glass-doored top.</p>
+                        <p>Antique hutch with glass-doored top. $400.</p>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4">
                         <img src="/images/40.JPG" class="img-responsive"/>
-                        <p>Antique dresser with dovetailed drawers.</p>
+                        <p>Antique dresser with dovetailed drawers from late 1800s/early 1900s. $400.</p>
                     </div>
                     <div class="col-md-4">
                         <img src="/images/37.JPG" class="img-responsive"/>
                         <p>Floor lamp.</p>
                     </div>
+                    <div class="col-md-4">
+                        <img src="/images/23.JPG" class="img-responsive"/>
+                        <p>Gently used couch. $200</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="/images/42.JPG" class="img-responsive"/>
+                    <p>Floor lamp</p>
                 </div>
             </div>
             <div role="tabpanel" class="tab-pane" id="knickknacks">
